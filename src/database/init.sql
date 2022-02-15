417389ed696cf75440dfe7b15861f9292f72aeb8
miahbates <miahbates@icloud.com>
2022-02-15 16:27:16 +0000

    updated init.sql file, created products and orders table with inserts
    Co-authored-by: Juliette <53294140+julietteorpen@users.noreply.github.com>
    Co-authored-by: Miah <78933903+miahbates@users.noreply.github.com>
    Co-authored-by: Paolo <15671939+paologhidoni@users.noreply.github.com>

--- src/database/init.sql
+++ src/database/init.sql
@@ -1,22 +1,22 @@
 BEGIN;
 
-DROP TABLE IF EXISTS users, products CASCADE;
-
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  email VARCHAR(30),
-  password TEXT NOT NULL
-);
+DROP TABLE IF EXISTS products, orders CASCADE;
 
 CREATE TABLE products (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(100),
-  title VARCHAR(30),
-  product_type VARCHAR(30),
-  description VARCHAR(140),
-  price VARCHAR(10),
-  seller_id INTEGER REFERENCES devpop_users(id)
+  color VARCHAR(15) NOT NULL,
+  type VARCHAR(10) NOT NULL, 
+  price INTEGER NOT NULL,
+  size TEXT NOT NULL,
+  stock INTEGER NOT NULL,
+  img TEXT NOT NULL
+);
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(100),
+  quantity INTEGER NOT NULL, 
+  product_id INTEGER REFERENCES products(id)
 );
 
 -- CREATE TABLE sessions (
@@ -26,12 +26,18 @@ CREATE TABLE products (
 
 
 
-INSERT INTO users (name, email, password) VALUES
-  ('Milly', 'milly@milly.com', 123),
-  ('Orian', 'orian@orian.com', 123),
-  ('Juliette', 'juliette@juliette.com', 123),
-  ('Miah', 'miah@miah.com', 123)
+INSERT INTO products (color, type, price, size, stock, img) VALUES
+  ('green', 'standard', 40.00, 'M', 1, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
+  ('white', 'standard', 40.00, 'S', 1, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
+  ('blue', 'platform', 50.00, 'S', 1, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
+  ('red', 'platform', 50.00, 'S', 1, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
+  ('yellow', 'boot', 60.00, 'S', 1, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
+  ('purple', 'boot', 60.00, 'S', 1, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919')
 ;
 
+
+INSERT INTO orders (username, quantity, product_id) VALUES
+('juliette', 2)
+;
 
 COMMIT;
