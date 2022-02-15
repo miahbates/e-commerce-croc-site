ce40eb6eb575eb6e1c7268589c142abd7a090ee1
neprojo <53294140+neprojo@users.noreply.github.com>
2022-02-15 17:16:14 +0000

    started db query
    Co-authored-by: Miah 78933903+miahbates@users.noreply.github.com Co-authored-by: Paolo 15671939+paologhidoni@users.noreply.github.com

---
+++ src/database/model.js
@@ -0,0 +1,12 @@
+// import db from "./connection.js";
+
+// function getProducts() {
+//   const GET_PRODUCTS = `SELECT * FROM products`;
+//   return db.query(GET_PRODUCTS).then((data) => {
+//     console.log("data", data);
+//     console.log("data rows", data.rows);
+//     return data.rows;
+//   });
+// }
+
+// export { getProducts };
