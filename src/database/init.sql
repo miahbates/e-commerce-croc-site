6278e8058512249e4f3a31a4499b7ad7f124ce5e
neprojo <53294140+neprojo@users.noreply.github.com>
2022-02-16 11:16:46 +0000

    created clickable li to hold products
    Co-authored-by: Miah 78933903+miahbates@users.noreply.github.com Co-authored-by: Paolo 15671939+paologhidoni@users.noreply.github.com

--- src/database/init.sql
+++ src/database/init.sql
@@ -4,6 +4,7 @@ DROP TABLE IF EXISTS products, orders CASCADE;
 
 CREATE TABLE products (
   id SERIAL PRIMARY KEY,
+  title VARCHAR (20) NOT NULL,
   color VARCHAR(15) NOT NULL,
   type VARCHAR(10) NOT NULL, 
   price INTEGER NOT NULL,
@@ -24,13 +25,13 @@ CREATE TABLE orders (
 --   data JSON NOT NULL
 -- );
 
-INSERT INTO products (color, type, price, size, stock, img) VALUES
-  ('green', 'standard', 40.00, '{"size":["small","medium","large"]}', 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
-  ('white', 'standard', 40.00, '{"size":["small","medium","large"]}', 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
-  ('blue', 'platform', 50.00, '{"size":["small","medium","large"]}', 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
-  ('red', 'platform', 50.00, '{"size":["small","medium","large"]}', 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
-  ('yellow', 'boot', 60.00, '{"size":["small","medium","large"]}', 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
-  ('purple', 'boot', 60.00, '{"size":["small","medium","large"]}', 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919');
+INSERT INTO products (title, color, type, price, size, stock, img) VALUES
+  ('Classic Green Crocs','green', 'classic', 40.00, '{"size":["small","medium","large"]}', 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
+  ('Classic White Crocs', 'white', 'classic', 40.00, '{"size":["small","medium","large"]}', 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
+  ('Platform Blue Crocs','blue', 'platform', 50.00, '{"size":["small","medium","large"]}', 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
+  ('Platform Red Crocs','red', 'platform', 50.00, '{"size":["small","medium","large"]}', 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
+  ('Yellow Boot Crocs', 'yellow', 'boot', 60.00, '{"size":["small","medium","large"]}', 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
+  ('Purple Boot Crocs','purple', 'boot', 60.00, '{"size":["small","medium","large"]}', 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919');
 
 
 
