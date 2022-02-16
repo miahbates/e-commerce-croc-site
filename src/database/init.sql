5b47b53ac2f6633e3827784ea1043197d0f63f82
Paolo <15671939+paologhidoni@users.noreply.github.com>
2022-02-16 10:28:25 +0000

    Create new database, work on home page
    Create new database to then hide .env file, start displaying products on home page.
    
    Co-authored-by: Juliette <53294140+julietteorpen@users.noreply.github.com>
    Co-authored-by: Miah <78933903+miahbates@users.noreply.github.com>

--- src/database/init.sql
+++ src/database/init.sql
@@ -24,20 +24,18 @@ CREATE TABLE orders (
 --   data JSON NOT NULL
 -- );
 
-
-
 INSERT INTO products (color, type, price, size, stock, img) VALUES
-  ('green', 'standard', 40.00, {size:['small','medium','large']}, 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
-  ('white', 'standard', 40.00, {size:['small','medium','large']}, 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
-  ('blue', 'platform', 50.00, {size:['small','medium','large']}, 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
-  ('red', 'platform', 50.00, {size:['small','medium','large']}, 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
-  ('yellow', 'boot', 60.00, {size:['small','medium','large']}, 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
-  ('purple', 'boot', 60.00, {size:['small','medium','large']}, 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919')
-;
+  ('green', 'standard', 40.00, '{"size":["small","medium","large"]}', 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
+  ('white', 'standard', 40.00, '{"size":["small","medium","large"]}', 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
+  ('blue', 'platform', 50.00, '{"size":["small","medium","large"]}', 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
+  ('red', 'platform', 50.00, '{"size":["small","medium","large"]}', 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
+  ('yellow', 'boot', 60.00, '{"size":["small","medium","large"]}', 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919'),
+  ('purple', 'boot', 60.00, '{"size":["small","medium","large"]}', 10, 'https://images-ext-1.discordapp.net/external/LhiRp0jJKqKBQQ_7irYjtQ2PpOW4J5TTGG2b0468LVs/https/media.crocs.com/images/t_pdpzoom/f_auto%252Cq_auto/products/203591_7A9_ALT100/crocs?width=1108&height=919');
+
 
 
 INSERT INTO orders (username, quantity) VALUES
-('juliette', 2)
-;
+('juliette', 2);
+
 
 COMMIT;
